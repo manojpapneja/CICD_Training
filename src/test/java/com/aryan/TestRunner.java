74db813282fe2af2128be0f300b566c55f411590
manojpapneja <70954269+manojpapneja@users.noreply.github.com>
2025-03-05 17:19:20 +0530

    Update TestRunner.java

--- src/test/java/com/aryan/TestRunner.java
+++ src/test/java/com/aryan/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources", // Path to your feature files directory
-        glue = "com.aryan"         // Package containing your step definition classes
+        glue = "com.aryan",        // Package containing your step definition classes
         plugin = {"pretty", "html:target/cucumber-reports"}
 )
 public class TestRunner {

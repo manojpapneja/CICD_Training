64b740de5cbb7c1cc666a6bc95125f79c24fde0e
Manu <mmanojpapneja@gmail.com>
2025-03-10 13:10:13 +0530

    Test webhook

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,7 +2,7 @@ package runners;
 
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-
+// Test webhook
 @CucumberOptions(
         features = "src/test/resources/features",  // Make sure this points to your feature files directory
         glue = "stepDefinitions",  // Package where your step definitions are located
